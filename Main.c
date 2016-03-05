b8b0171e4e9306fe6b0be4fb13af1e071d31546e
r1k4rd0 <rahv88@gmail.com>
2016-03-05 04:02:24 -0600

    Added files via upload
    Aproximación de 2*arcsin(x) usando Serie de Taylor con n unidades de trabajo de 50 términos cada una.

--- Main.c
+++ Main.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
-#include <ucontext.h>
-#include <signal.h>
-#include <sys/time.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include "myThread.h"
+#include <math.h>
 
-void trabajo(int cantidadTrabajo) {
-
+/**
+* Aproxima arcsen(x) mediante Serie de Taylor
+*  @arg x Variable dependiente de la función
+*  @arg n Cantidad de términos de la serie
+*/
+double arcsin(double x, int n)
+{
+    double fact,sum;
+    fact=sum=1.0;
+    int i;
+    for(i=1;i<=n;i++)
+    {
+	fact *= (2 * (double)i - 1)/(2 * (double)i );
+	double po = pow(x,(2*(double)i+1));
+	double factwo = po / (2*(double)i+1);
+	sum += fact * (pow(x,(2*(double)i+1)) / (2*(double)i+1));
+    }
+    return sum;
 }
-int main (int argc, char** argv){
 
+int main(void)
+{
+    int n;
+    printf("Cantidad de unidades de trabajo: ");
+    scanf("%d",&n);
+    printf("2 arcsin(1) = %lf\n",2*arcsin(1.0,n*50));
 }
